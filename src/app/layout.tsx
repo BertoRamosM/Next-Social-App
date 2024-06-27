a8de7cc764b301d9d82add1c5d411f5995c1a3f0
ARamos <albertramos902@gmail.com>
2024-06-27 14:48:46 +0000

    respnsive navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} >
+      <body className={`${inter.className}`} >
         <div className="w-full px-4 bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64 ">
           <Navbar />
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,38 @@
 import Link from 'next/link';
 import React from 'react'
 import MobileMenu from './MobileMenu';
+import Image from 'next/image';
+import HomeIcon from '@/app/icons/HomeIcon';
+import StoriesIcon from '@/app/icons/StoriesIcon';
+import FriendsIcon from '@/app/icons/FriendsIcon';
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
-      <div className="">
-        <Link href={"/"} className="text-red-500 font-bold text-xl">
-          BERTO<span className='border-2 border-red-500'>SOCIAL</span>
+      <div className="text-red-500 md:hidden lg:block w-20%]">
+        <Link href={"/"} className=" font-bold text-xl">
+          BERTO<span className="border-2 border-red-500">SOCIAL</span>
         </Link>
       </div>
 
-      <div className="hidden"></div>
+      <div className="hidden md:flex w-[50%]">
+        <div className="flex gap-6 text-gray-600">
+          <Link href="#" className="flex gap-2 hover:text-red-500 justify-center items-center">
+            <HomeIcon />
+            <span>Home</span>
+          </Link>
+          <Link href="/friends" className="flex gap-2 hover:text-red-500">
+            <FriendsIcon />
+            <span>Friends</span>
+          </Link>
+          <Link href="/stories" className="flex gap-2 hover:text-red-500">
+            <StoriesIcon />
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
 
-      <div className="">
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <MobileMenu />
       </div>
     </div>
