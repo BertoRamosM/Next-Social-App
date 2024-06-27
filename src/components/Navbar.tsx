07f4d9ae56a98b54ecb70c8db4bcee1e88158c25
ARamos <albertramos902@gmail.com>
2024-06-27 14:27:11 +0000

    created animation on small navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,11 @@ import MobileMenu from './MobileMenu';
 
 const Navbar = () => {
   return (
-    <div className='h-24 flex items-center justify-between'>
+    <div className="h-24 flex items-center justify-between">
       <div className="">
-        <Link href={'/'} className='text-red-500 font-bold text-xl'>BERTOSOCIAL</Link>
+        <Link href={"/"} className="text-red-500 font-bold text-xl">
+          BERTO<span className='border-2 border-red-500'>SOCIAL</span>
+        </Link>
       </div>
 
       <div className="hidden"></div>
