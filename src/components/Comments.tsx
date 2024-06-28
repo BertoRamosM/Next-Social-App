a47ce7e92adeddef23f926d10d722f1f88b68326
ARamos <albertramos902@gmail.com>
2024-06-28 14:05:14 +0000

    first version of feed section completed

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from "react";
 import Avatar from "../images/avatar.webp";
-import Image from 'next/image';
-import Emoji from "../images/emoji.webp"
-import Dots from '@/app/icons/Dots';
+import Image from "next/image";
+import Emoji from "../images/emoji.webp";
+import Dots from "@/app/icons/Dots";
+import Thumb from "@/app/icons/Thumb";
 
 const Comments = () => {
   return (
@@ -32,7 +33,7 @@ const Comments = () => {
       </div>
 
       <div className="">
-        <div className="">
+        <div className="flex gap-4 justify-between mt-6">
           <Image
             src={Avatar}
             alt="user avatar"
@@ -40,14 +41,29 @@ const Comments = () => {
             height={40}
             className="w-10 h-10 rounded-full"
           />
-          <div className=""></div>
-
+          <div className="flex flex-col gap-2 flex-1 ">
+            <span className="font-medium ">Ricardo Mars</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos officia facilis sit voluptatibus laboriosam tempore
+              expedita, tempora rerum et consequatur sed. Quam qui a quaerat
+              temporibus architecto corrupti debitis quidem!
+            </p>
+            <div className="flex items-center gap-8 text-xs text-gray-500">
+              <div className="flex items-center gap-4 mt-2 ">
+                <Thumb />
+                <span className="text-gray-300 "> | </span>
+                <span className="text-gray-500">123 Likes</span>
+                <div>Reply</div>
+              </div>
+            </div>
+          </div>
 
           <Dots />
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Comments
+export default Comments;
