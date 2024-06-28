677046ce276e2f989900e3e00a9b3c15677b30d7
ARamos <albertramos902@gmail.com>
2024-06-28 13:55:03 +0000

    working on post comment section

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,24 +1,51 @@
 import React from 'react'
-import Martial3 from "../images/Martial3.webp";
+import Avatar from "../images/avatar.webp";
 import Image from 'next/image';
 import Emoji from "../images/emoji.webp"
+import Dots from '@/app/icons/Dots';
 
 const Comments = () => {
   return (
     <div>
       <div className="flex items-center gap-4">
-        <Image src={Martial3} alt="user avatar" width={32} height={32} className='w-8 h-8 rounded-full'/>
-        <div className="flex items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full">
+        <Image
+          src={Avatar}
+          alt="user avatar"
+          width={32}
+          height={32}
+          className="w-8 h-8 rounded-full"
+        />
+        <div className="flex flex-1 items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full">
           <input
             type="text"
             placeholder="Write a comment..."
             className="bg-transparent outline-none flex-1"
           />
-          <Image src={Emoji} alt="emoji" width={16} height={16} className='rounded-full cursor-pointer'/>
+          <Image
+            src={Emoji}
+            alt="emoji"
+            width={16}
+            height={16}
+            className="rounded-full cursor-pointer"
+          />
         </div>
       </div>
 
-      <div className=""></div>
+      <div className="">
+        <div className="">
+          <Image
+            src={Avatar}
+            alt="user avatar"
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full"
+          />
+          <div className=""></div>
+
+
+          <Dots />
+        </div>
+      </div>
     </div>
   );
 }
