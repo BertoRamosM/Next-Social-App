bc119cdc77b9fdfb5428c4bb85d9ea211bbb58af
ARamos <albertramos902@gmail.com>
2024-07-05 15:11:56 +0000

    making friend request succeeded

--- src/components/rightMenu/FriendRequestList.tsx
+++ src/components/rightMenu/FriendRequestList.tsx
@@ -15,18 +15,20 @@ type RequestWithUser = FollowRequest & {
 
 const FriendRequestList = ({ requests }: { requests: RequestWithUser[] }) => {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       {requests.map((request) => (
         <div className="flex items-center justify-between" key={request.id}>
           <div className="flex items-center gap-4">
             <Image
-              src={Martial2}
+              src={request.sender.avatar || '/no-avatar.webp'}
               alt="user avatar"
               width={40}
               height={40}
               className="rounded-full object-cover w-10 h-10"
             />
-            <span className="font-semibold">{request.sender.name}</span>
+            <span className="font-semibold">
+              {request.sender.name && request.sender.surname ?
+              request.sender.name + " " + request.sender.surname : request.sender.username}</span>
           </div>
 
           <div className="flex justify-end gap-1">
