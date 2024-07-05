46b269c134ebf1114494d03b9d148aff62ff111b
ARamos <albertramos902@gmail.com>
2024-07-05 09:53:49 +0000

    optimistic hook applied to the db calls

--- src/components/rightMenu/UserInfoCardInteraction.tsx
+++ src/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { switchFollow } from "@/lib/actions";
-import { useState } from "react";
+import { useOptimistic, useState } from "react";
 
 interface UserInfoCardInteractionProps {
   userId: string;
@@ -39,20 +39,28 @@ const UserInfoCardInteraction: React.FC<UserInfoCardInteractionProps> = ({
       throw new Error('something went wrong')
     }
   }
+
+  const [optimisticFollow, switchOptimisticFollow] = useOptimistic(userState, (state) => ({
+    ...state,
+    following: state.following && false,
+        followingSent: !state.following && !state.followingSent ? true : false,
+    
+  })
+)
   return (
     <>
       <form action={follow}>
         <button className="w-full bg-red-500 text-green-100 text-sm rounded-md p-2">
-          {userState.following
+          {optimisticFollow.following
             ? "Following"
-            : userState.followingSent
+            : optimisticFollow.followingSent
             ? "Follow Request Sent"
             : "Follow"}
         </button>
       </form>
       <form action="" className="self-end">
         <span className="text-red-400 text-xs cursor-pointer">
-          {userState.blocked ? "Unblock User" : "Block User"}
+          {optimisticFollow.blocked ? "Unblock User" : "Block User"}
         </span>
       </form>
     </>
