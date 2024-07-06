0826562af7a5f2a28478ffcbb8ac28e9058eed8e
ARamos <albertramos902@gmail.com>
2024-07-06 10:15:26 +0000

    changed update user form styles

--- src/components/rightMenu/UpdateUser.tsx
+++ src/components/rightMenu/UpdateUser.tsx
@@ -28,10 +28,10 @@ const UpdateUser = ({ user }: { user: User }) => {
       </span>
 
       {isOpen && (
-        <div className="absolute h-screen w-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50 ">
+        <div className="absolute h-screen w-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50 py-12">
           <form
             action=""
-            className="p-12 bg-slate-800 rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3  relative"
+            className="p-12 bg-slate-800 rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative"
           >
             <h1 className="text-3xl">Update Profile</h1>
 
@@ -39,8 +39,8 @@ const UpdateUser = ({ user }: { user: User }) => {
               Use the header profile avatar to change the avatar or username
             </div>
 
-            <div className="flex flex-col gap-4 my-4">
-              <label>Cover Pictrue</label>
+            <div className="flex flex-col gap-2 my-4">
+              <label className="text-green-300 text-xs">Cover Pictrue</label>
 
               <div className="flex items-center gap-2 cursor-pointer">
                 <Image
@@ -54,8 +54,8 @@ const UpdateUser = ({ user }: { user: User }) => {
               </div>
             </div>
 
-            <div className="flex flex-wrap justify-between gap-2 xl:gap-4">
-              <div className="flex flex-col gap-4">
+            <div className="flex flex-wrap justify-between gap-4">
+              <div className="flex flex-col gap-2">
                 <label htmlFor="" className="text-xs text-green-300">
                   First name
                 </label>
@@ -66,7 +66,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 />
               </div>
 
-              <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
                 <label htmlFor="" className="text-xs text-green-300">
                   Surname
                 </label>
@@ -77,7 +77,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 />
               </div>
 
-              <div className="flex flex-col gap-4 w-full">
+              <div className="flex flex-col gap-2 w-full">
                 <label htmlFor="" className="text-xs text-green-300">
                   Description
                 </label>
@@ -89,50 +89,50 @@ const UpdateUser = ({ user }: { user: User }) => {
                     "Planet Earth is blue and there's nothing I can do"
                   }
                 />
+              </div>
 
-                <div className="flex flex-col gap-4">
-                  <label htmlFor="" className="text-xs text-green-300">
-                    City
-                  </label>
-                  <input
-                    type="text"
-                    placeholder={user.city || "New Red Rock"}
-                    className="bg-transparent border-b border-green-200 focus:outline-none p-2 text-xs"
-                  />
-                </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="text-xs text-green-300">
+                  City
+                </label>
+                <input
+                  type="text"
+                  placeholder={user.city || "New Red Rock"}
+                  className="bg-transparent border-b border-green-200 focus:outline-none p-2 text-xs"
+                />
+              </div>
 
-                <div className="flex flex-col gap-4">
-                  <label htmlFor="" className="text-xs text-green-300">
-                    School
-                  </label>
-                  <input
-                    type="text"
-                    placeholder={user.school || "Galactic University of Dust"}
-                    className="bg-transparent border-b border-green-200 focus:outline-none p-2 text-xs"
-                  />
-                </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="text-xs text-green-300">
+                  School
+                </label>
+                <input
+                  type="text"
+                  placeholder={user.school || "Galactic University of Dust"}
+                  className="bg-transparent border-b border-green-200 focus:outline-none p-2 text-xs"
+                />
+              </div>
 
-                <div className="flex flex-col gap-4">
-                  <label htmlFor="" className="text-xs text-green-300">
-                    Work
-                  </label>
-                  <input
-                    type="text"
-                    placeholder={user.work || "Mars Chocolate Inc."}
-                    className="bg-transparent border-b border-green-200 focus:outline-none p-2 text-xs"
-                  />
-                </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="text-xs text-green-300">
+                  Work
+                </label>
+                <input
+                  type="text"
+                  placeholder={user.work || "Mars Chocolate Inc."}
+                  className="bg-transparent border-b border-green-200 focus:outline-none p-2 text-xs"
+                />
+              </div>
 
-                <div className="flex flex-col gap-4">
-                  <label htmlFor="" className="text-xs text-green-300">
-                    Website
-                  </label>
-                  <input
-                    type="text"
-                    placeholder={user.website || "https://albertoramos.dev/"}
-                    className="bg-transparent border-b border-green-200 focus:outline-none p-2 text-xs"
-                  />
-                </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="" className="text-xs text-green-300">
+                  Website
+                </label>
+                <input
+                  type="text"
+                  placeholder={user.website || "https://albertoramos.dev/"}
+                  className="bg-transparent border-b border-green-200 focus:outline-none p-2 text-xs"
+                />
               </div>
 
               <button className="bg-red-500 p-2 mt-2 rounded-md text-white w-full hover:opacity-80">
@@ -141,7 +141,7 @@ const UpdateUser = ({ user }: { user: User }) => {
             </div>
 
             <div
-              className="absolute cursor-pointer text-lg right-2 top-2"
+              className="absolute cursor-pointer text-lg right-5 top-5 text-white"
               onClick={handleClose}
             >
               x
