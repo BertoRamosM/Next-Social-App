aa0cc5a0590c0097fb355caed8df7ea5235afbba
ARamos <albertramos902@gmail.com>
2024-07-06 09:05:27 +0000

    working on update profile modal

--- src/components/rightMenu/UpdateUser.tsx
+++ src/components/rightMenu/UpdateUser.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { User } from "@prisma/client"
+import Image from "next/image"
 import { useState } from "react"
 
 
@@ -24,7 +25,20 @@ const UpdateUser = ({ user }: { user: User }) => {
       {isOpen && <div className="absolute h-screen w-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50 ">
         <form action="" className="p-12 bg-slate-800 rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3  relative">
           
-          <h1>Update Profile </h1>
+          <h1>Update Profile</h1>
+
+          <div className="mt-4 text-xs text-green-300">
+            Use the header profile avatar to change the avatar or username
+          </div>
+
+          <div className="flex flex-col gap-4 my-4">
+            <label>Cover Pictrue</label>
+          </div>
+
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src={user.cover || '/no-cover.webp'} alt="user cover picture" width={48} height={32} className="w-12 h-8 rounded-md object-cover" />
+            <span className="text-xs underline text-red-500">Change</span>
+          </div>
           
           <div className="absolute cursor-pointer text-lg right-2 top-2" onClick={handleClose}>x</div>
         </form>
