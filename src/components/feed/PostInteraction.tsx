2175c6c89d4c064d41537dbb7a1a6b122d3269e0
ARamos <albertramos902@gmail.com>
2024-07-07 16:45:07 +0000

    created server action to switch between like

--- src/components/feed/PostInteraction.tsx
+++ src/components/feed/PostInteraction.tsx
@@ -3,25 +3,57 @@
 import Comment from '@/app/icons/Comment';
 import ShareIcon from '@/app/icons/ShareIcon';
 import Thumb from '@/app/icons/Thumb';
-import React from 'react'
+import ThumbFull from '@/app/icons/ThumbFull';
+import { switchLike } from '@/lib/actions';
+import { useAuth } from '@clerk/nextjs';
+import React, { useOptimistic, useState } from 'react'
 
 const PostInteraction = ({
   postId,
   likes,
   commentNumber,
 }: {
-  postId:number,
-  likes:string[],
+  postId: number,
+  likes: string[],
   commentNumber: number
 }) => {
+  
+  const { isLoaded, userId } = useAuth()
+
+  const [likeState, setLikeState] = useState({
+    likeCount: likes.length,
+    isLiked: userId ? likes.includes(userId) : false
+  })
+  
+  const [optimisticLike, switchoptimisticLike] = useOptimistic(likeState, (state, value) => {
+    return {
+      likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
+      isLiked: !state.isLiked
+    }
+  });
+
+  const likeAction = async () => {
+    switchoptimisticLike("")
+    try {
+      switchLike(postId)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
   return (
     <div className="flex items-center justify-between text-sm gap-4 my-4">
       <div className="flex gap-8">
         <div className="flex items-center gap-4 bg-slate-800 p-2 rounded-xl cursor-pointer">
-          <Thumb />
+          <form action={likeAction}>
+            <button>
+              {optimisticLike.isLiked ? <ThumbFull /> : <Thumb />}
+              </button>
+          </form>
           <span className="text-gray-300">|</span>
           <span className="text-gray-100">
-            182 <span className="hidden md:inline"> Likes</span>
+            {optimisticLike.likeCount}{" "}
+            <span className="hidden md:inline"> Likes</span>
           </span>
         </div>
         <div className="flex items-center gap-4 bg-slate-800 p-2 rounded-xl cursor-pointer">
